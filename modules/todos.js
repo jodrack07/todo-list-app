efca43fd79c35494aaf62b8efa832313fb882622
jodrack <drack.sir01@gmail.com>
2022-04-26 20:09:34 +0200

    Add sample todos file

---
+++ modules/todos.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    index: 1,
+    description: 'Practice Webpack correclty',
+    completed: true,
+  },
+  {
+    index: 3,
+    description: 'Watching mock interviews',
+    completed: false,
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
+import UI from '../modules/UI';
 import './style.css';
+
+const ui = new UI();
+
+// trigger the renderTodos method to display the li
+ui.renderTodos();
