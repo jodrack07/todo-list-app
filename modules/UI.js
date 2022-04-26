212413302d3e2661090ed5ecdaebc2fb1cd9c2cc
jodrack <drack.sir01@gmail.com>
2022-04-26 21:53:50 +0200

    Filter the todo list by index in ascending order

--- modules/UI.js
+++ modules/UI.js
@@ -4,20 +4,22 @@ const list = document.querySelector('.todos');
 
 export default class UI {
   renderTodos = () => {
-    todos.forEach((todo) => {
-      list.innerHTML += `<li class="item">
+    todos
+      .sort((a, b) => a.index - b.index)
+      .forEach((todo) => {
+        list.innerHTML += `<li class="item">
             <div class="todo">
                 <div class='todo-left'>
                 <input type="checkbox" class="check-box" name="checkbox" id= "${
                   todo.index
                 }" ${todo.completed ? 'checked' : ''}>
                 <p class='${todo.completed ? 'underline' : ''}'>${
-                  todo.description
-                }</p>
+          todo.description
+        }</p>
                 </div>
                 <i class="fa-solid fa-ellipsis-vertical"></i>
             </div>
         </li>`;
-    });
+      });
   };
 }
