5f33d4ff0c9aa24f9dac3a10cc0ded78c254073c
jodrack <drack.sir01@gmail.com>
2022-04-28 18:28:08 +0200

    Uncommet after debugging

--- src/index.js
+++ src/index.js
@@ -66,73 +66,73 @@ const editButtons = document.querySelectorAll('.edit');
 const deleteButtons = document.querySelectorAll('.delete');
 const toggleButtons = document.querySelectorAll('.fa-ellipsis');
 
-// const showSubActions = (selectedTodo) => {
-//   let taskMenu = selectedTodo.parentElement.lastElementChild;
-//   taskMenu.classList.add('show');
-//   document.addEventListener('click', (e) => {
-//     if (e.target.tagName !== 'I' || e.target !== selectedTodo) {
-//       taskMenu.classList.remove('show');
-//     }
-//   });
-// };
+const showSubActions = (selectedTodo) => {
+  let taskMenu = selectedTodo.parentElement.lastElementChild;
+  taskMenu.classList.add('show');
+  document.addEventListener('click', (e) => {
+    if (e.target.tagName !== 'I' || e.target !== selectedTodo) {
+      taskMenu.classList.remove('show');
+    }
+  });
+};
 
-// const editTask = (id, taskDescription) => {
-//   editId = id;
-//   isEditedTodo = true;
-//   // put the todo description into the input field
-//   taskInput.value = taskDescription;
-// };
+const editTask = (id, taskDescription) => {
+  editId = id;
+  isEditedTodo = true;
+  // put the todo description into the input field
+  taskInput.value = taskDescription;
+};
 
-// function deleteTask(id) {
-//   // remove one todo from the specified id
-//   todos.splice(id, 1);
-//   localStorage.setItem('todo-list', JSON.stringify(todos));
-//   showTodos();
-// }
+function deleteTask(id) {
+  // remove one todo from the specified id
+  todos.splice(id, 1);
+  localStorage.setItem('todo-list', JSON.stringify(todos));
+  showTodos();
+}
 
-// function updateStatus(selectedTodo) {
-//   // get tje paragraph
-//   let taskDescription = selectedTodo.parentElement.lastElementChild;
-//   if (selectedTodo.checked) {
-//     taskDescription.classList.add('completed');
-//     // update the status of the checked todo
-//     todos[selectedTodo.id].completed = true;
-//   } else {
-//     taskDescription.classList.remove('completed');
-//     // update the status of the checked todo
-//     todos[selectedTodo.id].completed = false;
-//   }
-//   // save changes into the local storage
-//   localStorage.setItem('todo-list', JSON.stringify(todos));
-// }
+function updateStatus(selectedTodo) {
+  // get tje paragraph
+  let taskDescription = selectedTodo.parentElement.lastElementChild;
+  if (selectedTodo.checked) {
+    taskDescription.classList.add('completed');
+    // update the status of the checked todo
+    todos[selectedTodo.id].completed = true;
+  } else {
+    taskDescription.classList.remove('completed');
+    // update the status of the checked todo
+    todos[selectedTodo.id].completed = false;
+  }
+  // save changes into the local storage
+  localStorage.setItem('todo-list', JSON.stringify(todos));
+}
 
-// // console.log(toggleButtons);
-// toggleButtons.forEach((toggleButton) =>
-//   toggleButton.addEventListener('click', function () {
-//     showSubActions(this);
-//   })
-// );
+// console.log(toggleButtons);
+toggleButtons.forEach((toggleButton) =>
+  toggleButton.addEventListener('click', function () {
+    showSubActions(this);
+  })
+);
 
-// editButtons.forEach((editButton) =>
-//   editButton.addEventListener('click', function () {
-//     const [id, description] = this.getAttribute('data-info').split(',');
-//     editTask(Number(id), String(description));
-//   })
-// );
+editButtons.forEach((editButton) =>
+  editButton.addEventListener('click', function () {
+    const [id, description] = this.getAttribute('data-info').split(',');
+    editTask(Number(id), String(description));
+  })
+);
 
-// deleteButtons.forEach((deleteButton) =>
-//   deleteButton.addEventListener('click', function () {
-//     const id = this.getAttribute('data-id');
-//     deleteTask(Number(id));
-//   })
-// );
+deleteButtons.forEach((deleteButton) =>
+  deleteButton.addEventListener('click', function () {
+    const id = this.getAttribute('data-id');
+    deleteTask(Number(id));
+  })
+);
 
-// statuses.forEach((input) =>
-//   input.addEventListener('click', function () {
-//     updateStatus(this);
-//   })
-// );
+statuses.forEach((input) =>
+  input.addEventListener('click', function () {
+    updateStatus(this);
+  })
+);
 
-// document.querySelector('#refresh').addEventListener('click', (e) => {
-//   location.reload();
-// });
+document.querySelector('#refresh').addEventListener('click', (e) => {
+  location.reload();
+});
