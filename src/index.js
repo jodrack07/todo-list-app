19bf0cbbbde705aa18274e7a2c436e67124718e8
jodrack <drack.sir01@gmail.com>
2022-04-28 18:00:59 +0200

    Add the showtodos function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,36 @@
-import UI from '../modules/UI.js';
 import './style.css';
 
-const ui = new UI();
+const taskInput = document.querySelector('.add-input');
+const taskBox = document.querySelector('.task-box');
 
-// trigger the renderTodos method to display the li
-ui.renderTodos();
+let editId;
+let isEditedTodo = false;
+
+// get data from the localstorage
+let todos = JSON.parse(localStorage.getItem('todo-list')) || [];
+
+function showTodos() {
+  taskBox.innerText = '';
+  todos.forEach((todo, index) => {
+    // apply the completed status iff the task/todo is completed
+    let isCompleted = todo.completed ? 'completed' : '';
+    let checked = isCompleted === 'completed' ? 'checked' : '';
+    taskBox.innerHTML += `
+            <li class="task">
+          <label for="${index}">
+            <input type="checkbox" ${checked} id="${index}" class="input"/>
+            <p class="${isCompleted}">${todo.description}</p>
+          </label>
+          <div class="actions">
+            <i class="fa-solid fa-ellipsis"></i>
+            <ul class="sub-actions">
+              <li data-info="${index}, ${todo.description}" class="edit"><i class="fa-solid fa-pen-to-square" id="edit"></i>Edit</li>
+              <li data-id="${index}" class="delete"><i class="fa-solid fa-trash-can" id="delete"></i>Delete</li>
+            </ul>
+          </div>
+        </li>  
+      `;
+  });
+}
+
+showTodos();
