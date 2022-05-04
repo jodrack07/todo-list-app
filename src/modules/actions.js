f1fffb3f517e28e94d5e3db621a3da8a3bb2bba6
jodrack <drack.sir01@gmail.com>
2022-05-04 07:57:56 +0200

    Import variables in the actions.js file for DRY resons

--- src/modules/actions.js
+++ src/modules/actions.js
@@ -1,34 +1,6 @@
-const taskInput = document.querySelector('.add-input');
-// get data from the localstorage
-const todos = JSON.parse(localStorage.getItem('todo-list')) || [];
-
-export const deleteTask = (id) => {
-  // remove one todo from the specified id
-  todos.splice(id, 1);
-  localStorage.setItem('todo-list', JSON.stringify(todos));
-  //   showTodos();
-  window.location.reload();
-};
-
-export const updateStatus = (selectedTodo) => {
-  // get tje paragraph
-  const taskDescription = selectedTodo.parentElement.lastElementChild;
-  if (selectedTodo.checked) {
-    taskDescription.classList.add('completed');
-    // update the status of the checked todo
-    todos[selectedTodo.id].completed = true;
-  } else {
-    taskDescription.classList.remove('completed');
-    // update the status of the checked todo
-    todos[selectedTodo.id].completed = false;
-  }
-  // save changes into the local storage
-  localStorage.setItem('todo-list', JSON.stringify(todos));
-  window.location.reload();
-};
-
-export const ClearAllCompleted = () => {
-  const newTodo = todos.filter((todo) => todo.completed === false);
-  localStorage.setItem('todo-list', JSON.stringify(newTodo));
-  window.location.reload();
-};
+import {
+  tasksArray,
+  taskSection,
+  userTask,
+  clearAllCompletedBtn,
+} from './variables.js';
