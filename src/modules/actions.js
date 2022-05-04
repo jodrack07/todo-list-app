d0750e7e88dd9b5260c9a59e675dab001feaacca
jodrack <drack.sir01@gmail.com>
2022-05-04 08:18:48 +0200

    Add renderTask static method

--- src/modules/actions.js
+++ src/modules/actions.js
@@ -1,3 +1,24 @@
 import { todos, taskBox, userTask, clearAllCompletedBtn } from './variables.js';
 import Task from './task.js';
 import LocalStorage from './localStorage.js';
+
+export default class Actions {
+  static renderTask = (todos) => {
+    taskBox.innerHTML = '';
+    for (let i = 0; i < todos.length; i += 1) {
+      taskBox.innerHTML += `
+  <div class="one-task" id=${i}>
+    <div class="data">
+      <input id=${i} class="checkBoxClass" type="checkbox" ${
+        todos[i].status ? 'checked' : ''
+      }>
+      <p id=${i} class="${todos[i].status ? 'checked' : ''} description">${
+        todos[i].task
+      }</p>
+    </div>
+    <span class="span">&cross;</span>
+  </div>
+  `;
+    }
+  };
+}
