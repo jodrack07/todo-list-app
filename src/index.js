8e541b6abafabddd6976c8624de6feede0ad5116
jodrack <drack.sir01@gmail.com>
2022-05-04 08:26:06 +0200

    Insert on press Enter key and Render all tasks

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 //eslint-disable-line
 import { userTask, todos } from './modules/variables.js';
 import Actions from './modules/actions.js';
+
+// populate the localStorage when the user press Enter
+userTask.addEventListener('keyup', (e) => {
+  if (e.keyCode === 13 && userTask.value) {
+    e.preventDefault();
+    Actions.addTask();
+  }
+});
+
+// render all tasks
+Actions.renderTask(todos);
