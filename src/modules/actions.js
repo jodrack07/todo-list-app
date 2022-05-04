5be02078bec455ccdf0882afb0233e01efcac4e9
jodrack <drack.sir01@gmail.com>
2022-05-04 12:49:55 +0200

    Implement the clearAll functionality

--- src/modules/actions.js
+++ src/modules/actions.js
@@ -1,30 +1,33 @@
 import { todos, taskBox, userTask, clearAllCompletedBtn } from './variables.js';
 import Task from './task.js';
 import LocalStorage from './localStorage.js';
+import Helpers from './helpers.js';
 
 export let update = false;
 export let tmp;
 
 export default class Actions {
+  // display todos from the localstorage
   static renderTask = (todos) => {
     taskBox.innerHTML = '';
     for (let i = 0; i < todos.length; i += 1) {
       taskBox.innerHTML += `
-  <div class="one-task" id=${i}>
-    <div class="data">
-      <input id=${i} class="checkBoxClass" type="checkbox" ${
-        todos[i].status ? 'checked' : ''
+  <li class="task" id=${i}>
+    <div class="task-details">
+      <input id=${i} class="input" type="checkbox" ${
+        todos[i].completed ? 'checked' : ''
       }>
-      <p id=${i} class="${todos[i].status ? 'checked' : ''} description">${
-        todos[i].task
-      }</p>
+      <p id=${i} class="${
+        todos[i].completed ? 'completed ' : ''
+      } description">${todos[i].task}</p>
     </div>
-    <span class="span">&cross;</span>
-  </div>
+    <i class="fa-solid fa-trash-can"></i>
+  </li>
   `;
     }
   };
 
+  // add a task into the localstorage
   static addTask = () => {
     if (!update) {
       const task = new Task(userTask);
@@ -39,5 +42,45 @@ export default class Actions {
       userTask.value = '';
       update = false;
     }
+  };
+
+  // remove a task into the localstorage
+  static removeTask = () => {
+    taskBox.addEventListener('click', (e) => {
+      if (e.target.classList.contains('fa-trash-can')) {
+        todos.splice(e.target.parentElement.id, 1);
+        userTask.value = '';
+        Actions.renderTask(todos);
+        Helpers.updateIndex();
+        LocalStorage.save(todos);
+      }
+
+      if (e.target.classList.contains('description')) {
+        userTask.focus();
+        userTask.value = e.target.innerHTML;
+        update = true;
+        tmp = e.target.id;
+      }
+    });
+  };
+
+  // toggle the completed status
+  static toggleCompleted = () => {
+    taskBox.addEventListener('change', (event) => {
+      if (event.target.className === 'input') {
+        todos[event.target.id].completed = !todos[event.target.id].completed;
+        LocalStorage.save(todos);
+        event.target.parentElement.classList.toggle('completed');
+      }
+    });
+  };
+
+  static deleteAllCompleted = () => {
+    clearAllCompletedBtn.addEventListener('click', () => {
+      todos = todos.filter((todo) => todo.completed === false);
+      Actions.renderTask(todos);
+      Helpers.updateIndex();
+      LocalStorage.save(todos);
+    });
   };
 }
