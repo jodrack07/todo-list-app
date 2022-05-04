4569e446d075aca0b64b968919a6cff6aeb55bf6
jodrack <drack.sir01@gmail.com>
2022-05-04 12:48:33 +0200

    Refactor the index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 //eslint-disable-line
 import { userTask, todos } from './modules/variables.js';
 import Actions from './modules/actions.js';
+import './style.css';
 
 // populate the localStorage when the user press Enter
 userTask.addEventListener('keyup', (e) => {
@@ -10,5 +11,11 @@ userTask.addEventListener('keyup', (e) => {
   }
 });
 
-// render all tasks
+// render all tasks/todos
 Actions.renderTask(todos);
+// remove a task/todo
+Actions.removeTask();
+// toggle the completed status of a task/todo
+Actions.toggleCompleted();
+// delete all completed task
+Actions.deleteAllCompleted();
